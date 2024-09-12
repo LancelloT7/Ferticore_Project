4a30bc9935c51f279bc346b68c66803f4518f751
LancelloT7 <matias7g@hotmail.com>
2024-09-12 16:19:40 -0300

    Tela de login adicionada

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    login = models.CharField(max_length=100)
+    senha = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.login
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+import json  # Certifique-se de importar json corretamente
+from django.contrib.auth.models import User
 
+
+from django.contrib.auth.hashers import make_password
 
 def cadastro(request):
+    if request.method == "POST":
+        login = request.POST.get('login')
+        senha = request.POST.get('senha')
+        
+        # Cria e salva um novo usuário com a senha criptografada
+        User.objects.create_user(username=login, password=senha)
+        
+        return HttpResponse("Usuário cadastrado com sucesso!")
     return render(request, 'cadastro.html')
 
+
+
+        
+
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+from django.shortcuts import render
+
 def auth(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        login = request.POST.get('login')
+        senha = request.POST.get('senha')
+        
+        # Verifica se o usuário está autenticado
+        user = authenticate(username=login, password=senha)
+        
+        if user is not None:
+            # Usuário autenticado com sucesso
+            return HttpResponse("Usuário logado com sucesso!")
+        else:
+            # Usuário não autenticado
+            return HttpResponse("Usuário não cadastrado ou senha incorreta.")
+
 
