c52751c7fda4951364d0017681168a9372e60cfd
LancelloT7 <matias7g@hotmail.com>
2024-09-12 09:41:40 -0300

    Base HTML incluida

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.cadastro),
+    path('cadastro/', views.cadastro, name ='cadastro'),
     path('auth/', views.auth, name ='auth' ),
 ]
